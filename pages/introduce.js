9ea36f08e95f2903f816bf413b9d4c86b0c7bd47
matsui-risa <matsui.risa.21@shizuoka.ac.jp>
2022-06-09 13:17:52 +0900

    first commit

---
+++ pages/introduce.js
@@ -0,0 +1,5 @@
+export default function Introduce() {
+  return (
+    <>THIS IS INTRODUCE PAGE</>
+  )
+}
---
+++ pages/sports.js
@@ -0,0 +1,7 @@
+export default function Sports() {
+  return (
+    <>
+      THIS IS SPORT PAGE
+    </>
+  )
+}
